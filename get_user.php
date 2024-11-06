7197bd5bf670c4b83f510c07058fc65c36f5a5a1
Akas Bagus Setiawan <146935384+AkasSakti@users.noreply.github.com>
2024-11-06 11:47:59 +0700

    Create get_user.php
    API-APIan

---
+++ get_user.php
@@ -0,0 +1,27 @@
+<?php
+$servername = "localhost";
+$username = "root"; // Ganti dengan username database Anda
+$password = ""; // Ganti dengan password database Anda
+$dbname = "skielinjeksi";
+
+// Membuat koneksi
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Cek koneksi
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM users";
+$result = $conn->query($sql);
+
+$users = array();
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        $users[] = $row;
+    }
+}
+
+echo json_encode($users);
+$conn->close();
+?>
